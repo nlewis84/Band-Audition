24596a20609e1760bd00e1c963b5ed78b0fa42bb
nllew <n.lewis.perc@gmail.com>
2021-02-09 11:12:36 -0600

    Welcome routes and Signup Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :instruments
   resources :players
   resources :audition_users
