4c8f3c1f719fb44f40a605675348900e96b35b0f
nllew <n.lewis.perc@gmail.com>
2021-02-09 22:54:19 -0600

    trying to get Instrument New to not make nil Player models on init

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,9 @@
 class Instrument < ApplicationRecord
   has_many :players
   belongs_to :audition
+
+  def remaining_spots
+    @players = self.players.count
+    self.available_spots - @players
+  end
 end
