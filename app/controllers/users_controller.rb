0d09c7788d29170b1917fabc14b19f74eb8ac292
nllew <n.lewis.perc@gmail.com>
2021-02-12 14:45:33 -0600

    fixed user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password, :first_name, :last_name)
+        params.require(:user).permit(:username, :password, :name)
     end
 
 end
