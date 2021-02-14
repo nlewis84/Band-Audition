82fba7afdfbb40dbd26807f126a7100e71f18f64
nllew <n.lewis.perc@gmail.com>
2021-02-14 10:00:11 -0600

    removed stray punctuation and moved Player Logic to Model

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,4 +6,15 @@ class Player < ApplicationRecord
 
   scope :belonging_to_current_audition, -> (audition) { where('audition_id = ?', audition)}
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def affirmative
+    self.virtual ? "Yes" : "No"
+  end
+
+  def school
+    self.audition.school
+  end
 end
