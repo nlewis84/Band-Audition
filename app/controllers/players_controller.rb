27835b148587dc1384671f2a5986961015a4d75c
nllew <n.lewis.perc@gmail.com>
2021-04-29 18:58:59 -0500

    edit player now properly selects current assigned instrument

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -39,7 +39,7 @@ class PlayersController < ApplicationController
         @audition = Audition.find_by(id: params[:audition_id])
         
         # this code should be looked at
-        @instrument = Instrument.find_by(id: params[:player][:instrument])
+        @instrument = Instrument.find_by(id: params[:player][:instrument].to_i)
         @instrument.players << @player
         
         # this code is fine
